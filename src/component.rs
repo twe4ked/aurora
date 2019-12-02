bcc6a55c5d985a928e067abc9d182f494815b0c7
Odin Dutton <odindutton@gmail.com>
2019-12-02 12:50:18 +1100

    Implement Display for Component

--- src/component.rs
+++ src/component.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, PartialEq)]
 pub enum Component {
     Char(char),
@@ -8,4 +10,33 @@ pub enum Component {
 pub enum CwdStyle {
     Default,
     Long,
+}
+
+impl fmt::Display for Component {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Component::Char(c) => write!(f, "{}", c),
+            Component::Cwd { style } => {
+                match std::env::current_dir() {
+                    Ok(dir) => {
+                        match style {
+                            // Replace the home directory portion of the path with "~/"
+                            CwdStyle::Default => {
+                                let home_dir =
+                                    dirs::home_dir().unwrap_or(std::path::PathBuf::new());
+                                match dir.strip_prefix(home_dir) {
+                                    Ok(dir) => write!(f, "~/{}", dir.display()),
+                                    // Unable to strip the prefix, fall back to full path
+                                    Err(_) => write!(f, "{}", dir.display()),
+                                }
+                            }
+                            CwdStyle::Long => write!(f, "{}", dir.display()),
+                        }
+                    }
+                    // unable to read current directory
+                    Err(_) => Ok(()),
+                }
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,11 @@
 mod component;
 mod parser;
 
-use component::Component;
-
 fn main() {
     let config = std::env::args().nth(1).unwrap_or("{cwd} $".into());
     let output = parser::parse(&config).unwrap().1;
 
     for component in output {
-        match component {
-            Component::Char(c) => print!("{}", c),
-            Component::Cwd { style } => {
-                match std::env::current_dir() {
-                    Ok(dir) => {
-                        match style {
-                            // Replace the home directory portion of the path with "~/"
-                            component::CwdStyle::Default => {
-                                let home_dir =
-                                    dirs::home_dir().unwrap_or(std::path::PathBuf::new());
-                                match dir.strip_prefix(home_dir) {
-                                    Ok(dir) => print!("~/{}", dir.display()),
-                                    // Unable to strip the prefix, fall back to full path
-                                    Err(_) => print!("{}", dir.display()),
-                                }
-                            }
-                            component::CwdStyle::Long => print!("{}", dir.display()),
-                        }
-                    }
-                    Err(_) => { /* unable to read current directory */ }
-                }
-            }
-        }
+        print!("{}", component);
     }
 }
