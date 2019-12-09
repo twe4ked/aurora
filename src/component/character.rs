747995818e29d425667619f32fab3458eed2c7c9
Odin Dutton <odindutton@gmail.com>
2019-12-09 17:01:22 +1100

    Add character docs

--- src/component/character.rs
+++ src/component/character.rs
@@ -1,5 +1,8 @@
 use crate::error::Error;
 
+/// # Notes
+///
+/// * Does not return `Err` and you can safely unwrap.
 pub fn display(c: &char) -> Result<String, Error> {
     Ok(format!("{}", c))
 }
