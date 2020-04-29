5ee38ed04bbe81cc4ceacdb977fea247d78f37c7
Odin Dutton <odindutton@gmail.com>
2020-04-29 12:31:04 +1000

    Remove duplicate comment

--- src/component/jobs.rs
+++ src/component/jobs.rs
@@ -1,7 +1,6 @@
 use crate::component::Component;
 
 pub fn display(jobs: Option<String>) -> Option<Component> {
-    // https://github.com/clap-rs/clap/issues/1740
     match jobs {
         Some(jobs) => Some(Component::Jobs(jobs)),
         None => None,
