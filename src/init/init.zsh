de7244b03559f90843c69b81cfb3de93e42fa6d8
Odin Dutton <odindutton@gmail.com>
2020-05-04 14:51:07 +1000

    Make use of add-zsh-hook API

--- src/init/init.zsh
+++ src/init/init.zsh
@@ -6,8 +6,6 @@ aurora_precmd() {
     PROMPT="$(__CMD__ run --config=__CONFIG__ --jobs="${__jobs:-__empty__}" --shell=zsh --status="$__status")"
 }
 
-[[ -z "${precmd_functions+1}" ]] && precmd_functions=()
+autoload -U add-zsh-hook
 
-if [[ ${precmd_functions[(ie)aurora_precmd]} -gt ${#precmd_functions} ]]; then
-    precmd_functions+=(aurora_precmd)
-fi
+add-zsh-hook precmd aurora_precmd
