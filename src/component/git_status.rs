ad63193ebc05c8e59c1ad9b6080f8f085743ae59
Odin Dutton <odindutton@gmail.com>
2020-05-07 17:09:33 +1000

    Show modified/new/deleted file status in git_status

--- src/component/git_status.rs
+++ src/component/git_status.rs
@@ -2,10 +2,27 @@ use crate::component::Component;
 use anyhow::Result;
 use git2::{Status, StatusEntry};
 
+// + New file added to the working tree
+// * File modified in the working tree
+// - File deleted from the working tree
 pub fn display() -> Result<Option<Component>> {
     if let Some(repo_status) = repo_status()? {
+        let mut output = String::new();
+
         if repo_status.is_wt_modified() {
-            return Ok(Some(Component::Computed("*".to_string())));
+            output.push('*');
+        }
+
+        if repo_status.is_wt_new() {
+            output.push('+');
+        }
+
+        if repo_status.is_wt_deleted() {
+            output.push('-');
+        }
+
+        if !output.is_empty() {
+            return Ok(Some(Component::Computed(output)));
         }
     }
     Ok(None)
