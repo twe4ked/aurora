ab4d18593d199a7a19235a47f88694689222a100
Odin Dutton <odindutton@gmail.com>
2020-05-11 10:01:27 +1000

    The ID here implements display

--- src/component/git_commit.rs
+++ src/component/git_commit.rs
@@ -8,17 +8,10 @@ pub fn display(context: &Context) -> Option<Component> {
             if head.is_err() {
                 return None;
             }
-            head.unwrap().peel_to_commit().ok().map(|commit| {
-                Component::Computed(
-                    commit
-                        .id()
-                        .as_bytes()
-                        .iter()
-                        .fold(String::new(), |acc, b| acc + &format!("{:02x}", b))
-                        [0..7]
-                        .to_owned(),
-                )
-            })
+            head.unwrap()
+                .peel_to_commit()
+                .ok()
+                .map(|commit| Component::Computed(format!("{}", commit.id())[0..7].to_owned()))
         }
         None => None,
     }
