44eea60d8d1906e41523a705dae5d97650f8ec7e
Odin Dutton <odindutton@gmail.com>
2020-05-12 17:35:49 +1000

    Extract stash_count()

--- src/component/git_stash.rs
+++ src/component/git_stash.rs
@@ -1,19 +1,26 @@
 use crate::component::Component;
 use crate::Context;
 
+use git2::Repository;
+
 pub fn display(context: &mut Context) -> Option<Component> {
-    match context.git_repository_mut() {
-        Some(ref mut r) => {
-            let mut count = 0;
-            let x = r.stash_foreach(|_, _, _| {
-                count += 1;
-                true
-            });
-            if x.is_err() || count == 0 {
-                return None;
-            }
-            Some(Component::Computed(format!("{}+", count)))
-        }
-        None => None,
+    let repository = context.git_repository_mut()?;
+    let count = stash_count(repository)?;
+
+    if count == 0 {
+        None
+    } else {
+        Some(Component::Computed(format!("{}+", count)))
     }
+}
+
+fn stash_count(repository: &mut Repository) -> Option<usize> {
+    let mut count = 0;
+    repository
+        .stash_foreach(|_, _, _| {
+            count += 1;
+            true
+        })
+        .ok()?;
+    Some(count)
 }
