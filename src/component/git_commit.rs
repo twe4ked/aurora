f23bc6a5f0047ff6cfaf4ff9f9253b52980db698
Odin Dutton <odindutton@gmail.com>
2020-05-13 17:51:48 +1000

    Simplify git_commit

--- src/component/git_commit.rs
+++ src/component/git_commit.rs
@@ -2,17 +2,10 @@ use crate::component::Component;
 use crate::Context;
 
 pub fn display(context: &Context) -> Option<Component> {
-    match context.git_repository() {
-        Some(r) => {
-            let head = r.head();
-            if head.is_err() {
-                return None;
-            }
-            head.unwrap()
-                .peel_to_commit()
-                .ok()
-                .map(|commit| Component::Computed(format!("{}", commit.id())[0..7].to_owned()))
-        }
-        None => None,
-    }
+    let repository = context.git_repository()?;
+    repository.head().ok().and_then(|head| {
+        head.peel_to_commit()
+            .ok()
+            .map(|commit| Component::Computed(format!("{}", commit.id())[0..7].to_owned()))
+    })
 }
